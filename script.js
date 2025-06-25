e1c77859f2f742c9962e4743c66e4103e723f8fc
Sushil Jaishi <sushiljaishi@gmail.com>
2025-06-25 15:22:34 +0545

    hardcoded copyright year was replaced with javascript current year

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links")
+  const icon = document.querySelector(".hamburger-icon")
+  menu.classList.toggle("open")
+  icon.classList.toggle("open")
 }
+
+const currentYear = new Date().getFullYear()
+document.getElementById("year").textContent = currentYear
